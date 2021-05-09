99243d40111685e04ec468b41fc1752232a74be1
Jaga <jagaranga@gmail.com>
2021-05-09 13:22:14 +0530

    Removing unused imports

--- src/main/java/me/jaga/basics/springboot/gradle/domain/Customer.java
+++ src/main/java/me/jaga/basics/springboot/gradle/domain/Customer.java
@@ -1,8 +1,6 @@
 package me.jaga.basics.springboot.gradle.domain;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.util.Objects;
 
--- src/test/java/me/jaga/basics/springboot/gradle/controller/SampleAppControllerIntegrationTest.java
+++ src/test/java/me/jaga/basics/springboot/gradle/controller/SampleAppControllerIntegrationTest.java
@@ -1,6 +1,5 @@
 package me.jaga.basics.springboot.gradle.controller;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
